c365e403637238a846a466107a682a511d774db9
JoelRamos22 <128657225+JoelRamos22@users.noreply.github.com.>
2024-10-23 00:12:35 -0500

    feat: :sparkles: Header with animation and scroll transitions finished

---
+++ src/components/button.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function Button({ array }) {
+    const [selectedIndex, setSelectedIndex] = useState(0); // El primer botón (índice 0) es seleccionado por defecto.
+
+    const handleButtonClick = (index) => {
+        setSelectedIndex(index);
+    };
+
+    return (
+        array.map((item, index) => {
+            const isSelected = index === selectedIndex;
+            return (
+                <button
+                    key={index}
+                    onClick={() => handleButtonClick(index)}
+                    className={`rounded-xl px-6 text-lg py-2 text-center 
+                        ${isSelected ? 'bg-[#E0B6FF] text-[#2E004E]' : 'bg-white text-black'}
+                    `}
+                >
+                    {item.text}
+                </button>
+            );
+        })
+    );
+}
+
